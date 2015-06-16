fe8eeb78d6ec17212e2b3d907f544bd915a9706b
劉 松然 <ryu_shozen@cyberagent.co.jp>
2015-06-16 11:44:52 +0900

    v0.8

--- EverydayNBA/AppDelegate.m
+++ EverydayNBA/AppDelegate.m
@@ -17,6 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
 
     self.viewController = [[ViewController alloc]init];
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
--- EverydayNBA/ViewController.m
+++ EverydayNBA/ViewController.m
@@ -15,7 +15,13 @@
 #import <SVProgressHUD.h>
 #import "ENTableViewCell.h"
 
+static void *AUMediaPlaybackCurrentTimeObservationContext = &AUMediaPlaybackCurrentTimeObservationContext;
+static void *AUMediaPlaybackDurationObservationContext = &AUMediaPlaybackDurationObservationContext;
+static void *AUMediaPlaybackTimeValidityObservationContext = &AUMediaPlaybackTimeValidityObservationContext;
+
 @interface ViewController (){
+    NSUInteger _currentItemDuration;
+    
     UIView          *playerView;
     UITableView     *videoTable;
     NSMutableArray  *videoList;
@@ -24,10 +30,20 @@
     UIButton        *fullscrenButton;
     
     BOOL            isFullScreen;
+    BOOL            isShowControlView;
+    BOOL _playbackTimesAreValid;
+    
+    UIButton        *controlView;
+    UIButton        *playButton;
+    UISlider        *seekBar;
+    
+    UIView          *navigationView;
+    UILabel         *title;
 }
 
 @property (nonatomic) VideoView *playbackView;
 @property (nonatomic, strong) NSTimer *progressTimer;
+@property (nonatomic, strong) NSTimer *controlTimer;
 
 @end
 
@@ -41,12 +57,31 @@
     [backImage setImage:[UIImage imageNamed:@"backgroundImage"]];
     [self.view addSubview:backImage];
     
+    //navigation title
+    navigationView = [[UIView alloc]initWithFrame:CGRectMake(0, 20, [UIScreen mainScreen].bounds.size.width, 40)];
+    [self.view addSubview:navigationView];
+    
+    title = [[UILabel alloc]initWithFrame:CGRectMake(0, 11, navigationView.frame.size.width, 20)];
+    title.font = [UIFont fontWithName:@"Baskerville" size:20];
+    title.textColor = [UIColor whiteColor];
+    title.text = @"Everyday Basketball";
+    title.textAlignment = NSTextAlignmentCenter;
+    [navigationView addSubview:title];
+    
+    //init control view
+    controlView = [[UIButton alloc]init];
+    playButton = [[UIButton alloc]init];
+    seekBar = [[UISlider alloc]init];
+    fullscrenButton= [[UIButton alloc]init];
+
     //video view
     self.playbackView = [[VideoView alloc]initWithFrame:CGRectMake(0, 64, self.view.frame.size.width, self.view.frame.size.width/16*9)];
     [self.view addSubview:self.playbackView];
     [self setControlView];
+    [self initControlView];
     
-    videoTable = [[UITableView alloc]initWithFrame:CGRectMake(0, self.playbackView.frame.origin.y + self.playbackView.frame.size.height + 50, self.view.frame.size.width, self.view.frame.size.height - self.playbackView.frame.origin.y - self.playbackView.frame.size.height - 50)];
+    
+    videoTable = [[UITableView alloc]initWithFrame:CGRectMake(0, self.playbackView.frame.origin.y + self.playbackView.frame.size.height, self.view.frame.size.width, self.view.frame.size.height - self.playbackView.frame.origin.y - self.playbackView.frame.size.height)];
     videoTable.separatorStyle = UITableViewCellSeparatorStyleNone;
     videoTable.delegate = self;
     videoTable.dataSource = self;
@@ -55,7 +90,6 @@
     
     //test data
     videoList = [[NSMutableArray alloc]init];
-
 }
 
 - (void)viewDidLoad {
@@ -67,11 +101,23 @@
     urlString = @"http://www.nba.co.jp/nba/video";
     request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];
     [_webView loadRequest:request];
+    
+    [self musicPlayerStateChanged:nil];
 }
 
 -(void) viewDidAppear:(BOOL)animated{
     //parse data
     [SVProgressHUD showWithStatus:@"Loading..." maskType:SVProgressHUDMaskTypeGradient];
+    
+    //notification center
+    AUMediaPlayer *player = [self player];
+    [player addObserver:self forKeyPath:@"currentPlaybackTime" options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew context:AUMediaPlaybackCurrentTimeObservationContext];
+    [player addObserver:self forKeyPath:@"duration" options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew context:AUMediaPlaybackDurationObservationContext];
+    [player addObserver:self forKeyPath:@"playbackTimesAreValid" options:NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew context:AUMediaPlaybackTimeValidityObservationContext];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(musicPlayerStateChanged:)
+                                                 name:kAUMediaPlaybackStateDidChangeNotification
+                                               object:nil];
 }
 
 -(void) webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error{
@@ -110,15 +156,12 @@
     
     
     if (![tempString isEqualToString:@""]) {
-        AUMediaPlayer *player = [AUMediaPlayer sharedInstance];
-        NSError *error;
         topItem.remotePath = tempString;
-        [player playItem: topItem error:&error];
-        [self setPlayerLayer];
-        [player play];
+        
+        [self initView];
+        
     }
-    
-    [SVProgressHUD dismiss];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -131,15 +174,53 @@
     [layer setPlayer:[self player].player];
 }
 
+-(void) initView{
+    AUMediaPlayer *player = [AUMediaPlayer sharedInstance];
+    NSError *error;
+    [player playItem: topItem error:&error];
+    [self setPlayerLayer];
+    [player play];
+    [playButton setImage:[UIImage imageNamed:@"pause"] forState:UIControlStateNormal];
+
+    //controlview
+    [self initControlView];
+}
+
+-(void) initControlView{
+    isShowControlView = NO;
+    [controlView setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0.0]];
+    for(UIView *view in [controlView subviews]){
+        [view setHidden:YES];
+    }
+}
+
 - (AUMediaPlayer *)player {
     return [AUMediaPlayer sharedInstance];
 }
 
 - (void) setControlView{
-    [fullscrenButton removeFromSuperview];
-    fullscrenButton = [[UIButton alloc]initWithFrame:CGRectMake(self.playbackView.frame.size.width - 24, self.playbackView.frame.size.height - 24, 20, 20)];
+    [controlView removeFromSuperview];
+    
+    [controlView setFrame:self.playbackView.frame];
+    [controlView addTarget:self action:@selector(toggleControlView) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:controlView];
+    
+    //play button
+    [playButton setFrame:CGRectMake(controlView.frame.size.width/2 - 32, controlView.frame.size.height/2 - 32, 64, 64)];
+    [playButton addTarget:self action:@selector(togglePlayAction) forControlEvents:UIControlEventTouchUpInside];
+    [controlView addSubview:playButton];
+    
+    //seek
+    [seekBar setFrame:CGRectMake(5, controlView.frame.size.height - 25, controlView.frame.size.width - 40, 20)];
+    seekBar.tintColor = [UIColor orangeColor];
+    [seekBar addTarget:self action:@selector(didSlide) forControlEvents:UIControlEventValueChanged];
+    [controlView addSubview:seekBar];
+    
+    //    [fullscrenButton removeFromSuperview];
+    //fllscreen
+    [fullscrenButton setFrame:CGRectMake(controlView.frame.size.width - 26, controlView.frame.size.height - 26, 20, 20)];
     [fullscrenButton setImage:[UIImage imageNamed:@"fullscreen"] forState:UIControlStateNormal];
-    [self.playbackView addSubview:fullscrenButton];
+    [controlView addSubview:fullscrenButton];
     [fullscrenButton addTarget:self action:@selector(toggleFullscreen) forControlEvents:UIControlEventTouchUpInside];
 }
 
@@ -189,20 +270,9 @@
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
     [SVProgressHUD showWithStatus:@"Loading..." maskType:SVProgressHUDMaskTypeGradient];
-    
-    if (indexPath.row==0) {
-        AUMediaPlayer *player = [AUMediaPlayer sharedInstance];
-        NSError *error;
-        //
-        
-        [player playItem:topItem error:&error];
-        [player play];
-    }
-    else{
-        VideoItem *item = [videoList objectAtIndex:indexPath.row];
-        request = [NSURLRequest requestWithURL:[NSURL URLWithString:item.uid]];
-        [_webView loadRequest:request];
-    }
+    VideoItem *item = [videoList objectAtIndex:indexPath.row];
+    request = [NSURLRequest requestWithURL:[NSURL URLWithString:item.uid]];
+    [_webView loadRequest:request];
 }
 
 - (void)tableView:(UITableView *)tableView willDisplayCell:(ENTableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
@@ -217,16 +287,99 @@
 
 - (void)updateDownloadProgress {
     AUMediaLibrary *library = [AUMediaPlayer sharedInstance].library;
+    NSLog(@"%lu", (unsigned long)library.downloadingItems.count);
+
     if (!library.downloadingItems && library.downloadingItems.count == 0) {
         [self.progressTimer invalidate];
         self.progressTimer = nil;
     }
     if (self.progressTimer == nil) {
-        self.progressTimer = [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(updateDownloadProgress) userInfo:nil repeats:YES];
+        self.progressTimer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(updateDownloadProgress) userInfo:nil repeats:YES];
     }
     for (id<AUMediaItem> item in library.downloadingItems) {
         NSLog(@"%@", [item uid]);
     }
 }
+
+#pragma show control view
+-(void) updateControlViewStatus{
+    
+}
+
+-(void) toggleControlView{
+    if (isShowControlView) {
+        isShowControlView = NO;
+        [controlView setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0.0]];
+        
+        for(UIView *view in [controlView subviews]){
+            [view setHidden:YES];
+        }
+    } else {
+        isShowControlView = YES;
+        [controlView setBackgroundColor:[UIColor colorWithRed:0 green:0 blue:0 alpha:0.3]];
+        for(UIView *view in [controlView subviews]){
+            [view setHidden:NO];
+        }
+    }
+}
+
+#pragma notification method
+- (void)musicPlayerStateChanged:(NSNotification *)notification {
+    [self updateButtonsForStatus:[[AUMediaPlayer sharedInstance] playbackStatus]];
+}
+- (void)updateButtonsForStatus:(AUMediaPlaybackStatus)status {
+    if (status == 1) {
+        [SVProgressHUD dismiss];
+    }
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    
+    if (context == AUMediaPlaybackTimeValidityObservationContext) {
+        BOOL playbackTimesValidaity = [change[NSKeyValueChangeNewKey] boolValue];
+        _playbackTimesAreValid = playbackTimesValidaity;
+
+    } else if (context == AUMediaPlaybackCurrentTimeObservationContext) {
+        NSUInteger currentPlaybackTime = [change[NSKeyValueChangeNewKey] integerValue];
+        [self updatePlaybackProgressSliderWithTimePassed:currentPlaybackTime];
+    } else if (context == AUMediaPlaybackDurationObservationContext) {
+        NSUInteger currentDuration = [change[NSKeyValueChangeNewKey] integerValue];
+        _currentItemDuration = currentDuration;
+
+    } else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
+- (void)updatePlaybackProgressSliderWithTimePassed:(NSUInteger)time {
+    if (_playbackTimesAreValid && _currentItemDuration > 0) {
+        seekBar.value = (float)time/(float)_currentItemDuration;
+    } else {
+        seekBar.value = 0.0;
+    }
+}
+
+-(void) didSlide {
+    [[self player] seekToMoment:seekBar.value];
+}
+
+-(void) togglePlayAction{
+    AUMediaPlayer *player = [AUMediaPlayer sharedInstance];
+//    if (player.playbackStatus == AUMediaPlaybackStatusPlayerInactive || (topItem && ![[player.nowPlayingItem uid] isEqualToString:topItem.uid])) {
+//        NSError *error;
+//        [player playItem: topItem error:&error];
+//        [self setPlayerLayer];
+//        [player play];
+//
+//    } else
+    if (player.playbackStatus == AUMediaPlaybackStatusPlaying) {
+        [player pause];
+        [playButton setImage:[UIImage imageNamed:@"play"] forState:UIControlStateNormal];
+    } else {
+        [playButton setImage:[UIImage imageNamed:@"pause"] forState:UIControlStateNormal];
+        [player play];
+    }
+}
+
 
 @end
